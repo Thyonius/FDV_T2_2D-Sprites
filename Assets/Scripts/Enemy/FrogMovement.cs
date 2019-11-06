1606532643632fddf0b3d44c9b69f143b72aa902
Thyonius <pablosuarlo@gmail.com>
2019-11-06 17:12:09 +0000

    Initial commit

---
+++ Assets/Scripts/Enemy/FrogMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMovement : MonoBehaviour
+{
+    Animator frogAnim;
+
+    private void Awake()
+    {
+        frogAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        frogAnim.SetBool("Jump", true);
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        frogAnim.SetBool("Jump", false);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;            
+
+    Vector3 movement;                   
+    Animator anim;                      
+    Rigidbody2D playerRigidbody;          
+
+    SpriteRenderer playerSpriteRend;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerSpriteRend = GetComponent<SpriteRenderer>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Move(h);
+       
+        Animating(h);
+    }
+
+    void Move(float h)
+    {
+        movement.Set(h, 0f, 0f);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Animating(float h)
+    {
+        bool walking = h != 0f;
+
+        if (h >= 0.01)
+        {
+            playerSpriteRend.flipX = false;
+        }
+        if (h <= -0.01)
+        {
+            playerSpriteRend.flipX = true;
+        }
+
+        anim.SetBool("IsMoving", walking);
+    }
+}
